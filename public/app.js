894462062bae75155c217dff97fbac73e188dd51
Seth Randell <sethhrandell@gmail.com>
2019-10-02 17:01:11 -0400

    Added delete route for notes and got it working

--- public/app.js
+++ public/app.js
@@ -30,7 +30,7 @@ $(document).on("click", "p", function() {
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
+      $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Note</button>");
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
@@ -49,6 +49,35 @@ $(document).on("click", "#savenote", function() {
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+// When you click the savenote button
+$(document).on("click", "#deletenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+console.log("Delete button works");
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "DELETE",
     url: "/articles/" + thisId,
     data: {
       // Value taken from title input
