c7f730d6b21324f3d389bcb7f531a19ec8146262
Seth Randell <sethhrandell@gmail.com>
2019-10-05 15:25:22 -0400

    Adding the port for Heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -28,7 +28,11 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/nyTimesTech", { useNewUrlParser: true });
-mongoose.connect("mongodb://localhost/kicks_DB", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/kicks_DB", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kicks_DB";
+
+mongoose.connect(MONGODB_URI);
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
